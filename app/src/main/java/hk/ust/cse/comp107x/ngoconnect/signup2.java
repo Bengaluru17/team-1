c59af48a7e3fdd870c449f3bd59b8d6dbb2400d4
Padhavi B <padha31.b@gmail.com>
2017-07-08 15:43:39 +0530

    2nd commit

--- app/src/main/java/hk/ust/cse/comp107x/ngoconnect/signup2.java
+++ app/src/main/java/hk/ust/cse/comp107x/ngoconnect/signup2.java
@@ -21,28 +21,6 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-
-import android.content.Intent;
-import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
-import android.text.TextUtils;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
-
-import com.firebase.client.Firebase;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-
 public class signup2 extends AppCompatActivity
 {
     private FirebaseAuth auth;
@@ -172,7 +150,7 @@ public class signup2 extends AppCompatActivity
         mDatabase= FirebaseDatabase.getInstance().getReference();
 
         Log.d(LOG_TAG,"user");
-        User newUser = new User(userName, email);
+        Users newUser = new Users(userName, email);
         mDatabase.child("USERS").child(uid).setValue(newUser);
 
         /**
